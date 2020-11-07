d5a7de1d8c8913ed93d1c4026c57f3969d586e43
Gus <collarsoa@gmail.com>
2020-11-07 14:35:04 -0600

    Modification of the model to include the drug treatment after the population carrying capacity is initially reached.

--- Exercise11.R
+++ Exercise11.R
@@ -7,8 +7,8 @@
 r=0.1
 k=1000000
 
-#Generate an arbitrary time step
-t<-1:300
+#Generate an arbitrary time step that shows the carrying capacity being reached
+t<-1:1000
 
 #store output as a data frame, has one column for time, one for mutant population, one for normal population
 output<-matrix(data=NA, nrow=length(t), ncol=(3))
@@ -17,8 +17,13 @@ output[1,2]=1
 output[1,3]=99
 #for loop running the model of each populations growth, that stores the pop size in the matrix
 for(i in t[-1]){
-  output[i,2]=output[(i-1),2]+r*output[(i-1),2]*(1-(output[(i-1),2]+output[(i-1),3])/k)
-  output[i,3]=output[(i-1),3]+r*output[(i-1),3]*(1-(output[(i-1),3]+output[(i-1),3])/k)
+  if(t[i]<300){
+    output[i,2]=output[(i-1),2]+r*output[(i-1),2]*(1-(output[(i-1),2]+output[(i-1),3])/k)
+    output[i,3]=output[(i-1),3]+r*output[(i-1),3]*(1-(output[(i-1),3]+output[(i-1),3])/k)
+  }else{
+    output[i,2]=output[(i-1),2]+(r/2)*output[(i-1),2]*(1-(output[(i-1),2]+output[(i-1),3])/k)
+    output[i,3]=output[(i-1),3]+(-1*r)*output[(i-1),3]*(1-(output[(i-1),3]+output[(i-1),3])/k)
+  }
 }
 
 #plot results, need to make dataframe first to use ggplot
@@ -26,7 +31,7 @@ outputDF<-data.frame(time=output[,1], MutantPop=output[,2], NormalPop=output[,3]
 ggplot(data=outputDF, aes(x=time))+
   geom_line(aes(y=MutantPop))+
   geom_line(aes(y=NormalPop))+
-  ylab("Total pop")
+  ylab("Total pop")+
   theme_classic()
 
 
